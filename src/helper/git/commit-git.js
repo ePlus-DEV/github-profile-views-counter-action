2f27996d8562eb830d70e2a8ff1e28cf442e3b02
David Nguyen <h250694@gmail.com>
2024-01-29 11:48:34 +0700

    Based on the provided git diff, a possible commit message could be:
    Update INSIGHT_BOT_USERNAME and INSIGHT_BOT_EMAIL

--- src/helper/git/commit-git.js
+++ src/helper/git/commit-git.js
@@ -1,8 +1,8 @@
 const core = require('@actions/core');
 const git = require('../../core/git');
 let commitGit = function () {
-    let INSIGHT_BOT_USERNAME = 'github-actions[bot]';
-    let INSIGHT_BOT_EMAIL = '41898282+github-actions[bot]@users.noreply.github.com';
+    let INSIGHT_BOT_USERNAME = process.env.INSIGHT_BOT_USERNAME || 'eplus-bot';
+    let INSIGHT_BOT_EMAIL = process.env.INSIGHT_BOT_EMAIL || 'bot@eplus.dev';
     let commit = async function (message) {
         core.info(`Git Commit ${message}`)
         try {
